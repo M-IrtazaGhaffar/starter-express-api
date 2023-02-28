8f3f728ebf05f422b7b4f71df18fa567759859c6
Irtaza Ghaffar <93589223+M-IrtazaGhaffar@users.noreply.github.com>
2023-02-28 12:21:30 +0500

    Update 1

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require("express");
+const app = express();
+const port = 5000;
+
+// Routes
+app.get("/", (req, res) => res.send("Hello World!"));
+
+// Listening
+app.listen(port, () => console.log(`App listening on port ${port}!`));
